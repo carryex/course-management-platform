a1552599a7bda0fcaeb19fbae4185e0b0daa6f79
Daniil Pokinsokha <pokinsokha@gmail.com>
2024-08-14 14:19:26 +0000

    Move routes to constant

---
+++ app/constants/routes.ts
@@ -0,0 +1,8 @@
+// app/constants/routes.ts
+
+export const ROUTES = {
+  HOME: { path: '/', label: 'Course Management Platform' },
+  COURSES: { path: '/courses', label: 'Courses' },
+  ABOUT: { path: '/about', label: 'About' },
+  CONTACT: { path: '/contact', label: 'Contact' },
+};
