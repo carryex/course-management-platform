b2721cf0e2ab53c3aa6336d630cd51022e6d9ab2
Daniil Pokinsokha <pokinsokha@gmail.com>
2024-08-17 12:08:16 +0000

    Removed default stories

--- app/components/sample.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-
-test('renders a simple message', () => {
-  render(<div>Hello, Jest!</div>);
-  const message = screen.getByText(/Hello, Jest!/i);
-  expect(message).toBeInTheDocument();
-});
