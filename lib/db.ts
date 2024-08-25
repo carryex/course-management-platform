8a37083891324cfe0eb7994f075d44f46027cb1d
Daniil Pokinsokha <pokinsokha@gmail.com>
2024-08-25 13:51:21 +0000

    formattting

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
 // lib/db.ts
 
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 declare global {
   var prisma: PrismaClient | undefined;
@@ -8,4 +8,4 @@ declare global {
 
 export const db = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
